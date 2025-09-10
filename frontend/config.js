1efe82c625800c6ad6ba496d39af3591e9a021fc
Abdumalik <155809438+abdumalik06432530@users.noreply.github.com>
2025-09-10 05:56:51 -0700

    Update config.js
    Signed-off-by: Abdumalik <155809438+abdumalik06432530@users.noreply.github.com>

--- frontend/config.js
+++ frontend/config.js
@@ -2,5 +2,5 @@
   // Default runtime config. This file can be generated from .env using the
   // generate-config.js script (npm run gen-config).
   window.__ENV = window.__ENV || {};
-  window.__ENV.BASE_URL = window.__ENV.BASE_URL || 'telecom-icvr.vercel.app';
+  window.__ENV.BASE_URL = window.__ENV.BASE_URL || 'https://ethiotelecome.vercel.app/api';
 })(window);
